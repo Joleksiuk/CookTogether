d7bff1252bbfdcb7cd8d6c2e5bdfaa72976aab7b
Joleksiuk <julkaoleksiuk26@gmail.com>
2021-12-30 16:38:13 +0100

    Removed CTGAuthenticationDB and moved it's queries to CookTogetherDB

--- CookTogether/Startup.cs
+++ CookTogether/Startup.cs
@@ -37,7 +37,7 @@ namespace CookTogether
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                   Configuration.GetConnectionString("Authentication")));
+                   Configuration.GetConnectionString("Default")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                  .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
