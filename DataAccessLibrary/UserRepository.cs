d3d9da58cf1c90a58b23c8a3f9d39cf86e7a9f02
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-01-22 13:38:27 +0100

    UserRepository update after rebase

--- DataAccessLibrary/UserRepository.cs
+++ DataAccessLibrary/UserRepository.cs
@@ -21,12 +21,32 @@ namespace DataAccessLibrary
             string sql = @"select * from [dbo].[AspNetUsers]";
             return _db.LoadData<UserModel, dynamic>(sql, new { });
         }
+        public Task<List<UserModel>> GetUsersExcludingUser(UserModel user)
+        {
+            string sql = @"select * from [dbo].[AspNetUsers] WHERE Id != '" + user.Id + "'";
+            return _db.LoadData<UserModel, dynamic>(sql, new { });
+        }
 
         public Task InsertUser(UserModel user)
         {
             string sql = @"insert into [dbo].[AspNetUsers] (Id, Username, PasswordHash)
                             VALUES (@Id, @UserName, @PasswordHash)";
             return _db.SaveData(sql, user);
+        }
+
+        public async Task<UserModel> GetUserById(string id)
+        {
+            List<UserModel> users = await GetUsers();
+            UserModel foundUser = new UserModel();
+            foreach (var user in users)
+            {
+                if (user.Id == id)
+                {
+                    foundUser = user;
+                    break;
+                }
+            }
+            return foundUser;
         }
     }
 }
