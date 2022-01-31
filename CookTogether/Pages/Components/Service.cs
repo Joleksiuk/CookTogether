713ee49711a9271b4f2cf63bb7c51d652a6b6167
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-01-31 22:25:01 +0100

    Altering cosmetics

---
+++ CookTogether/Pages/Components/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookTogether.Pages.Components
+{
+    public class Service
+    {
+        public event EventHandler DoSomething;
+
+        public void DoSometing(string parameter)
+        {
+
+        }
+    }
+}
